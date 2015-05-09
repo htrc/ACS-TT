47a4e4e27ba161beb721d89fd6a2402376278aab
Boris Capitanu <borice@hotmail.com>
2015-05-09 12:01:52 -0500

    Added ability to include volume metadata in the CSV output by retrieving correct volume id from METS and querying SOLR

--- tools/philosofrequency/philosofrequency.py
+++ tools/philosofrequency/philosofrequency.py
@@ -4,8 +4,108 @@ from multiprocessing import Pool
 from functools import partial
 from contextlib import closing
 from zipfile import ZipFile
+from xml.etree.ElementTree import ElementTree
+from urllib import quote
 
-import sys, os, argparse, time, csv, re
+import sys, os, argparse, time, csv, re, urllib2, json
+
+METS_NS = { "METS": "http://www.loc.gov/METS/", "PREMIS": "info:lc/xmlns/premis-v2" }
+SOLR_QUERY_TPL = "http://chinkapin.pti.indiana.edu:9994/solr/meta/select?q=id:{}&fl=title,author,publishDate&wt=json"
+
+def findrelativefrequencies(text, keywords):
+    textfreqs = {}
+    relfreqs = {}
+
+    for keyword in keywords:
+        textfreqs[keyword] = 0
+
+    words = text.split()
+
+    for word in words:
+        for ignore in ignorechars:
+            if ignore in word:
+                word = word.replace(ignore, "")
+        if word in keywords:
+            textfreqs[word] += 1
+
+    wordcount = len(words)
+    relfreqs["WordCount"] = wordcount
+
+    relfreqtotal = 0
+    for key, value in textfreqs.items():
+        relfreqtotal += float(value) / float(wordcount)
+#        relfreqs[key] = float(relfreqtotal)
+
+    relfreqs["RelFreqSum"] = relfreqtotal
+    relfreqs["Frequencies"] = textfreqs
+
+    return relfreqs
+
+def log_freqs(result, file, ):
+    relfrequencies[file] = result
+
+def get_htrc_id(zippath):
+    folder, zipfile = os.path.split(zippath)
+    metsfile = os.path.splitext(zipfile)[0] + ".mets.xml"
+    metspath = os.path.join(folder, metsfile)
+    metsxml = ElementTree(file=metspath)
+    htrc_id = metsxml.find(".//PREMIS:objectIdentifierValue", METS_NS).text
+
+    return htrc_id
+
+def get_meta(htrc_id):
+    meta = {}
+    query = SOLR_QUERY_TPL.format(quote(htrc_id))
+    url = urllib2.urlopen(query)
+    try:
+        response = json.loads(url.read())
+        doc = response["response"]["docs"][0]
+        meta["Title"] = "\n".join(doc["title"])
+        meta["Author"] = "\n".join(doc["author"])
+        meta["Year"] = "\n".join(doc["publishDate"])
+    finally:
+        url.close()
+
+    return meta
+
+def processzipvolume(zippath, keywords):
+    root, file = os.path.split(zippath)
+
+    htrc_id = get_htrc_id(zippath)
+    meta = get_meta(htrc_id)
+
+    print("Finding frequencies for " + file)
+
+    text = ""
+
+    with ZipFile(zippath) as zipfile:
+        for filename in [zipentry.filename for zipentry in zipfile.infolist() if zipentry.filename.lower().endswith(".txt")]:
+            text += "\n" + zipfile.read(filename)
+
+    relfreqs = findrelativefrequencies(text, keywords)
+    relfreqs.update(meta)
+
+    return relfreqs
+
+def processtxtvolume(textpath, keywords):
+    root, file = os.path.split(textpath)
+    print("Finding frequencies for " + file)
+
+    #single core functionality commented out
+    #log_freqs(findrelativefrequencies(textpath), textpath)
+
+    with open(textpath) as textfile:
+        text = textfile.read().lower()
+
+    relfreqs = findrelativefrequencies(text, keywords)
+
+    relfreqs["Title"] = "temp"
+    relfreqs["Author"] = "temp"
+    relfreqs["Year"] = "temp"
+
+    return relfreqs
+
+######## main() ##############
 
 starttime = time.time()
 ignorechars = [".",",","?",")","(","\"",":",";","'s"]
@@ -46,75 +146,15 @@ print("Keywords read: {}".format(len(keywords)))
 print("Reading volume files.")
 index = 0
 
-def findrelativefrequencies(text):
-    textfreqs = {}
-    relfreqs = {}
-
-    for keyword in keywords:
-        textfreqs[keyword] = 0
-
-    words = text.split()
-
-    for word in words:
-        for ignore in ignorechars:
-            if ignore in word:
-                word = word.replace(ignore, "")
-        if word in keywords:
-            textfreqs[word] += 1
-
-    wordcount = len(words)
-
-    relfreqs["Title"] = "temp"
-    relfreqs["Author"] = "temp"
-    relfreqs["Year"] = "temp"
-    relfreqs["WordCount"] = wordcount
-
-    relfreqtotal = 0
-    for key, value in textfreqs.items():
-        relfreqtotal += float(value) / float(wordcount)
-#        relfreqs[key] = float(relfreqtotal)
-
-    relfreqs["RelFreqSum"] = relfreqtotal
-    relfreqs["Frequencies"] = textfreqs
-
-    return relfreqs
-
-def log_freqs(result, volid):
-    relfrequencies[volid] = result
-
-def processzipvolume(zippath):
-    root, file = os.path.split(zippath)
-    print("Finding frequencies for " + file)
-
-    text = ""
-
-    with ZipFile(zippath) as zipfile:
-        for filename in [zipentry.filename for zipentry in zipfile.infolist() if zipentry.filename.lower().endswith(".txt")]:
-            text += "\n" + zipfile.read(filename)
-
-    return findrelativefrequencies(text)
-
-def processtxtvolume(textpath):
-    root, file = os.path.split(textpath)
-    print("Finding frequencies for " + file)
-
-    #single core functionality commented out
-    #log_freqs(findrelativefrequencies(textpath), textpath)
-
-    with open(textpath) as textfile:
-        text = textfile.read().lower()
-
-    return findrelativefrequencies(text)
-
 pool = Pool()
 try:
     for root, dirs, files in os.walk(textdir):
         for volume in [file for file in files if file.lower().endswith("." + fileformat)]:
             volumepath = os.path.join(root, volume)
             if bool(re.search("\.zip$", volume, re.I)):
-                pool.apply_async(processzipvolume, (volumepath,), callback=partial(log_freqs, volid=volume))
+                pool.apply_async(processzipvolume, (volumepath, keywords,), callback=partial(log_freqs, file=volumepath))
             elif bool(re.search("\.txt$", volume, re.I)):
-                pool.apply_async(processtxtvolume, (volumepath,), callback=partial(log_freqs, volid=volume))
+                pool.apply_async(processtxtvolume, (volumepath, keywords,), callback=partial(log_freqs, file=volumepath))
 finally:
     pool.close()
     pool.join()
