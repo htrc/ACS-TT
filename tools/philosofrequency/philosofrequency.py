6e6c1d0821ac7654efc6d27d7ee98e19cfe162b5
Boris Capitanu <borice@hotmail.com>
2015-10-09 00:23:26 -0500

    Updated Laura's keyword list and modified code to support multiword keywords

--- tools/philosofrequency/philosofrequency.py
+++ tools/philosofrequency/philosofrequency.py
@@ -8,35 +8,69 @@ from urllib import quote
 from urllib2 import Request, urlopen, URLError, HTTPError
 from pairtree import pairtree_path
 from io import open
+from cStringIO import StringIO
+from itertools import islice
+from collections import Counter
+from datetime import timedelta
 
-import sys, os, argparse, time, csv, re, json
+import sys, os, argparse, time, csv, regex, json
 
 SOLR_QUERY_TPL = "http://chinkapin.pti.indiana.edu:9994/solr/meta/select?q=id:{}&fl=title,author,publishDate&wt=json&omitHeader=true"
-PAIRTREE_REGEX = re.compile(r"(?P<libid>[^/]+)/pairtree_root/(?P<ppath>.+)/(?P<cleanid>[^/]+)\.[^.]+$")
+PAIRTREE_REGEX = regex.compile(r"(?P<libid>[^/]+)/pairtree_root/(?P<ppath>.+)/(?P<cleanid>[^/]+)\.[^.]+$")
+EOL_HYPHEN_REGEX = regex.compile(ur"(?m)(\S*\p{L})-\s*\n(\p{L}\S*)\s*")
+PUNCT_REGEX = regex.compile(ur"[^\P{P}-']+")
+CONTRACTION_REGEX = regex.compile(ur"'s\b")
+
+
+def window(seq, n=2):
+    "Returns a sliding window (of width n) over data from the iterable"
+    "   s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...                   "
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result    
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
+
+def clean_and_normalize(text):
+    text = EOL_HYPHEN_REGEX.sub(r"\1\2\n", text)  # join end-of-line hyphenated words
+    text = PUNCT_REGEX.sub(" ", text)  # remove all punctuation except hyphens and apostrophes
+    text = CONTRACTION_REGEX.sub("", text)  # remove the 's contraction from words
+    text = text.lower()
+    return text
 
 def findrelativefrequencies(text, keywords):
-    textfreqs = {}
+    textfreqs = Counter()
     relfreqs = {}
 
-    for keyword in keywords:
-        textfreqs[keyword] = 0
-
-    words = text.split()
-
-    for word in words:
-        for ignore in ignorechars:
-            if ignore in word:
-                word = word.replace(ignore, "")
-        if word in keywords:
-            textfreqs[word] += 1
-
+    words = clean_and_normalize(text).split()
     wordcount = len(words)
     relfreqs["WordCount"] = wordcount
+
+    if wordcount > 1:
+        counter = Counter()
+        for w1, w2 in window(words):
+            if w1 in tokens:
+                counter[(w1,)] += 1
+                counter[(w1, w2)] += 1
+
+        if w2 in tokens:
+            counter[(w2,)] += 1
+
+        for k in keywords:
+            if len(k) == 1:
+                textfreqs[k] = counter[k]
+            else:
+                textfreqs[k] = min(counter[part] for part in window(k))
+    else:
+        for w in words:
+            if (w,) in keywords:
+                textfreqs[(w,)] = 1
 
     relfreqtotal = 0
     for key, value in textfreqs.items():
         relfreqtotal += float(value) / float(wordcount)
-#        relfreqs[key] = float(relfreqtotal)
 
     relfreqs["RelFreqSum"] = relfreqtotal
     relfreqs["Frequencies"] = textfreqs
@@ -45,9 +79,7 @@ def findrelativefrequencies(text, keywords):
 
 def log_freqs(result, file, ):
     # TODO: Title, Author, etc.
-    sortedfreqs = []
-    for keyword in keywords:
-        sortedfreqs.append(result["Frequencies"][keyword])
+    textfreqs = result["Frequencies"]
 
     # add default values for missing attributes
     for attr in ["VolID", "Title", "Author", "Year"]:
@@ -61,7 +93,7 @@ def log_freqs(result, file, ):
          result["Year"].encode('utf-8'),
          result["WordCount"],
          result["RelFreqSum"]
-        ] + sortedfreqs)
+        ] + [textfreqs[keyword] for keyword in keywords])
 
 def get_htrc_id(zippath):
     pt_parts = PAIRTREE_REGEX.search(zippath)
@@ -105,11 +137,15 @@ def processzipvolume(zippath):
 
     print("Finding frequencies for: " + htrc_id)
 
-    text = ""
+    text = StringIO()
 
     with ZipFile(zippath, 'r') as zipfile:
-        for filename in [zipentry.filename for zipentry in zipfile.infolist() if zipentry.filename.lower().endswith(".txt")]:
-            text += "\n" + zipfile.read(filename).decode("utf-8")
+        page_files = [zipentry.filename for zipentry in zipfile.infolist() if zipentry.filename.lower().endswith(".txt")]
+        for filename in sorted(page_files):
+            text.write(' ')
+            text.write(zipfile.read(filename).decode('utf-8'))
+    
+    text = text.getvalue()
 
     relfreqs = findrelativefrequencies(text, keywords)
     relfreqs.update(meta)
@@ -125,7 +161,7 @@ def processtxtvolume(textpath):
     #log_freqs(findrelativefrequencies(textpath), textpath)
 
     with open(textpath, encoding='utf-8') as textfile:
-        text = textfile.read().lower()
+        text = textfile.read()
 
     relfreqs = findrelativefrequencies(text, keywords)
 
@@ -136,81 +172,73 @@ def processtxtvolume(textpath):
 
     return relfreqs
 
-def pretty_time(seconds):
-    sign_string = '-' if seconds < 0 else ''
-    seconds = abs(int(seconds))
-    days, seconds = divmod(seconds, 86400)
-    hours, seconds = divmod(seconds, 3600)
-    minutes, seconds = divmod(seconds, 60)
-    if days > 0:
-        return '%s%dd%dh%dm%ds' % (sign_string, days, hours, minutes, seconds)
-    elif hours > 0:
-        return '%s%dh%dm%ds' % (sign_string, hours, minutes, seconds)
-    elif minutes > 0:
-        return '%s%dm%ds' % (sign_string, minutes, seconds)
-    else:
-        return '%s%ds' % (sign_string, seconds)
 
 ######## main() ##############
 
-starttime = time.time()
-ignorechars = [".",",","?",")","(","\"",":",";","'s"]
+def main():
+    starttime = time.time()
+    ignorechars = [".", ",", "?", ")", "(", "\"", ":", ";", "'s"]
 
-parser = argparse.ArgumentParser(
-    description='Calculate the frequency of keywords in text files. Creates an ouput file ranking by relative frequency.',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter
-)
-parser.add_argument('--words', dest='keywordfilename', default='keywords.txt',
-                    help='A text file containing the line-break separate keywords to use')
-parser.add_argument('--texts', dest='textdir',  default='texts',
-                    help='The folder of the volumes to examine')
-parser.add_argument('--format', dest='fileformat', metavar='txt|zip', default='txt',
-                    help='Specifies the format of the volume files')
+    parser = argparse.ArgumentParser(
+        description='Calculate the frequency of keywords in text files. Creates an output file ranking by relative frequency.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--words', dest='keywordfilename', default='keywords.txt',
+                        help='A text file containing the line-break separate keywords to use')
+    parser.add_argument('--texts', dest='textdir',  default='texts',
+                        help='The folder of the volumes to examine')
+    parser.add_argument('--format', dest='fileformat', choices=['txt', 'zip'], default='txt',
+                        help='Specifies the format of the volume files')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-textdir = args.textdir
-keywordfilename = args.keywordfilename
-fileformat = args.fileformat
+    textdir = args.textdir
+    keywordfilename = args.keywordfilename
+    fileformat = args.fileformat
 
-if fileformat not in ["txt", "zip"]:
-    sys.exit("Invalid file format specified. Aborting.")
-if not os.path.exists(keywordfilename):
-    sys.exit("Invalid keywords file path. Aborting.")
-elif not os.path.exists(textdir):
-    sys.exit("Invalid volumes folder path. Aborting.")
+    if not os.path.exists(keywordfilename):
+        sys.exit("Invalid keywords file path. Aborting.")
+    elif not os.path.exists(textdir):
+        sys.exit("Invalid volumes folder path. Aborting.")
 
-keywords = []
+    keywords = set()
 
-print("Reading keywords.")
-with open(keywordfilename, encoding='utf-8') as keywordfile:
-    for line in keywordfile.readlines():
-        keywords.append(line.strip(' \t\n\r').lower())
-print("Keywords read: {}".format(len(keywords)))
+    print("Reading keywords.")
+    with open(keywordfilename, encoding='utf-8') as keywordfile:
+        for line in keywordfile.readlines():
+            words = line.lower().split()
+            if not words: continue
+            keywords.add(tuple(words))
+    print("Keywords read: {}".format(len(keywords)))
 
-print("Reading volume files.")
-index = 0
-filecount = 0
+    tokens = set(token for keyword in keywords for token in keyword)
 
-pool = Pool()
-with open('output.csv', 'wb') as csvfile:
-    outputcsv = csv.writer(csvfile)
-    outputcsv.writerow(["Filename", "VolID", "Title", "Author", "Year", "WordCount", "RelFreqSum"] + [k.encode('utf-8') for k in keywords])
+    print("Reading volume files.")
+    index = 0
+    filecount = 0
 
-    try:
-        for root, dirs, files in os.walk(textdir):
-            for volume in [file for file in files if file.lower().endswith("." + fileformat)]:
-                volumepath = os.path.join(root, volume)
-                filecount += 1
-                if bool(re.search("\.zip$", volume, re.I)):
-                    pool.apply_async(processzipvolume, (volumepath,), callback=partial(log_freqs, file=volumepath))
-                elif bool(re.search("\.txt$", volume, re.I)):
-                    pool.apply_async(processtxtvolume, (volumepath,), callback=partial(log_freqs, file=volumepath))
-    finally:
-        pool.close()
-        pool.join()
+    pool = Pool()
+    with open('output.csv', 'wb') as csvfile:
+        outputcsv = csv.writer(csvfile)
+        outputcsv.writerow(["Filename", "VolID", "Title", "Author", "Year", "WordCount", "RelFreqSum"] + [" ".join(k).encode('utf-8') for k in keywords])
 
-print("Files processed: {}".format(filecount))
+        try:
+            for root, dirs, files in os.walk(textdir):
+                for volume in [file for file in files if file.lower().endswith("." + fileformat)]:
+                    volumepath = os.path.join(root, volume)
+                    filecount += 1
+                    if bool(regex.search("\.zip$", volume, regex.I)):
+                        pool.apply_async(processzipvolume, (volumepath,), callback=partial(log_freqs, file=volumepath))
+                    elif bool(regex.search("\.txt$", volume, regex.I)):
+                        pool.apply_async(processtxtvolume, (volumepath,), callback=partial(log_freqs, file=volumepath))
+        finally:
+            pool.close()
+            pool.join()
 
-elapsed = int(time.time() - starttime)
-print("Time elapsed: {}".format(pretty_time(elapsed)))
+    print("Files processed: {}".format(filecount))
+
+    elapsed = int(time.time() - starttime)
+    print("Time elapsed: {}".format(timedelta(seconds=elapsed)))
+
+if __name__ == '__main__':
+    main()
